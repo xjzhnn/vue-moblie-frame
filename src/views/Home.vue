fb45d6973374729496c774e20fd030ca4a04ab13
xjzhnn <38847221+xjzhnn@users.noreply.github.com>
2020-06-21 20:16:37 +0800

    增加样式初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="home">
     <h1>首页</h1>
 	<h3 @click="goAbout">跳到about页面</h3>
-	<div class="border-b box">我是测试1px</div>
+	<div class="border-b border-t box">我是测试1px</div>
+	<div class="box1"></div>
+	<div class="box2 border-all"></div>
   </div>
 </template>
 
@@ -22,5 +24,19 @@ export default {
 	.box{
 		height: 200px;
 		width: 200px;
+	}
+	.box1{
+		width: 100px;
+		height: 100px;
+		border:1px solid #000;
+		margin-top: 50px ;
+	}
+	.border-all:after{
+		border-radius: 10px;
+	}
+	.box2{
+		width:80%;
+		height: 45px;
+		margin:20px auto;
 	}
 </style>
